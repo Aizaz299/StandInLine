441d8f39561d678574f5d955f19020b1e3d4195c
Aizaz Ur Rehman <aziaz299@gmail.com>
2020-02-28 21:23:20 +0500

    Update code.js

--- code.js
+++ code.js
@@ -7,3 +7,5 @@ array=[1,2,3,4,5];
 console.log("Before calling function"+JSON.stringify(array));
 standInLine(array,6);
 console.log("After calling function"+JSON.stringify(array));
+
+![Annotation 2020-02-28 212150](https://user-images.githubusercontent.com/13184390/75565783-61b04380-5a70-11ea-83c8-b88f449f603d.png)
